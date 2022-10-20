9462d8b8e68a9d40945de51af7b6d50a7fab7af2
Michael Rea <MichaelReaNZ@gmail.com>
2022-10-20 13:20:53 +1300

    Half baked timer

--- ArenaBattle/Assets/Scripts/Managers/GameManager.cs
+++ ArenaBattle/Assets/Scripts/Managers/GameManager.cs
@@ -6,7 +6,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance;
+    private static GameManager Instance;
+    
+    //keep track of how long the game has been running
+    private TimeSpan timePlaying;
+    private bool isTimerRunning;
+    private float elapsedTime;
     
     public static event Action<GameState> OnGameStateChanged; 
     void Awake()
@@ -27,12 +32,33 @@ public class GameManager : MonoBehaviour
                 break;
             case GameState.GameOver:
                 break;
+            case GameState.ShrinkingArena:
+                break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(newGameState), newGameState, null);
         }
 
         OnGameStateChanged?.Invoke(newGameState);
+    }
+    
+    public void StartTimer()
+    {
+        isTimerRunning = true;
+        elapsedTime = 0f;
 
+        StartCoroutine(UpdateTimer() as string);
+    }
+    
+    private IEnumerable UpdateTimer()
+    {
+        while (isTimerRunning)
+        {
+            elapsedTime += Time.deltaTime;
+            timePlaying = TimeSpan.FromSeconds(elapsedTime);
+            string timePlayingStr = "Time: " + timePlaying.ToString("mm':'ss'.'ff");
+            Debug.Log(timePlayingStr);
+            yield return null;
+        }
     }
 
     // Start is called before the first frame update
@@ -44,15 +70,13 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
-    
-
 }
 
 public enum GameState
 {
     MainMenu,
     GameInProgress,
+    ShrinkingArena,
     GameOver
 }
