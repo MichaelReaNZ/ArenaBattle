1b1385ef91c4e1ac71120c335cabe56f9c08c8ab
Treedaze <50260710+Treedaze@users.noreply.github.com>
2022-10-27 14:33:55 +1300

    Bullets
    Bullets collide and destory themselves

--- ArenaBattle/Assets/Scripts/Item/Projectile.cs
+++ ArenaBattle/Assets/Scripts/Item/Projectile.cs
@@ -5,61 +5,9 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
 
-
-    float flightTime = 0.0f;
-    float damage = 0.0f;
-    Vector3 velocity = new Vector3(0.0f, 0.0f, 0.0f);
-
-    public float getVelocity
+private void OnCollisionEnter(Collision collision)
     {
-        get
-        {
-            return velocity.magnitude;
-        }
-    }
-
-    public float getDamage
-    {
-        get
-        {
-            return damage;
-        }
-    }
-
-    public float getFlightTime
-    {
-        get
-        {
-            return flightTime;
-        }
-    }
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-      
-    }
-/*    //Something about interacting with objects
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.GetComponent<Player>)
-        {
-            Player.TakeDamage(damage);
-        }
-    }*/
-    //something about destroying itself when hitting objects
-
-    public void Initialise(Vector3 _velocity, float _flightTime, float _damage)
-    {
-        velocity = _velocity;
-        flightTime = _flightTime;
-        damage = _damage;
+        Destroy(gameObject);
+        Debug.Log("object destroyed");
     }
 }
